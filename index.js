6a1b559f0656b3825f20da6b8a5e3ac32ae94d5d
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-08-17 14:38:56 +0200

    Added MongoDB Added bodyParser Added route /addPost

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const mongodb = require('mongodb');
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}`;
 const app = express();
 const port = process.env.PORT || 8080;
+
+/**
+ * Middleware
+ */
+app.use(bodyParser.json());
+const client = new MongoClient(uri);
+let db = await client.connect();
+
+/**
+ * Posts a new post.
+ */
+app.post('/addPost', (req, res) => {
+    let title = req.body.data.title;
+});
 
 app.listen(port, () => {
     console.log(`Listen on ${port}`);
